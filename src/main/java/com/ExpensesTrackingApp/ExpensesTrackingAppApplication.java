2fb36793db3123f6262c64d12ade3381b180843e
Anabela Santos <63252829+AnabelaSantos@users.noreply.github.com>
2023-03-19 18:41:47 +0000

    Initial commit

---
+++ src/main/java/com/ExpensesTrackingApp/ExpensesTrackingAppApplication.java
@@ -0,0 +1,13 @@
+package com.ExpensesTrackingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesTrackingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesTrackingAppApplication.class, args);
+	}
+
+}
